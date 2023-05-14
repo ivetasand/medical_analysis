186c7b6971d0cd44d777a92331706f5563a4cdff
veta <ivetasand@gmail.com>
2023-05-15 00:09:36 +0300

    refactoring, class creation, adding new functions

--- utils/constants/database.py
+++ utils/constants/database.py
@@ -3,38 +3,42 @@ import pathlib
 DATABASE_NAME = 'medical_analysis.sqlite'
 DATABASE_PATH = f'{pathlib.Path().resolve()}/medical_analysis.sqlite'
 
-ANALYSIS_VALUE_TYPE_TABLE_CREATION = ("CREATE TABLE IF NOT EXISTS analysis_value_type (\n"
-                                      "id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
-                                      "name TEXT NOT NULL,\n"
-                                      "abbreviation TEXT\n"
-                                      ");\n")
+ANALYSIS_VALUE_TYPE_TABLE_CREATION = (
+    "CREATE TABLE IF NOT EXISTS analysis_value_type (\n"
+    "id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+    "name TEXT NOT NULL,\n"
+    "abbreviation TEXT\n"
+    ");\n")
 
 LABORATORY_TABLE_CREATION = ("CREATE TABLE IF NOT EXISTS laboratory (\n"
                              "id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
                              "name TEXT NOT NULL\n"
                              ");\n")
 
-ANALYSIS_ITEM_TABLE_CREATION = ("CREATE TABLE IF NOT EXISTS analysis_item (\n"
-                                "id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
-                                "name TEXT NOT NULL,\n"
-                                "value_type_id INTEGER NOT NULL,\n"
-                                "FOREIGN KEY (value_type_id) REFERENCES analysis_value_type (id)\n"
-                                ");\n")
+# ANALYSIS_ITEM_TABLE_CREATION = ("CREATE TABLE IF NOT EXISTS analysis_item (\n"
+#                                 "id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+#                                 "value_type_id INTEGER NOT NULL,\n"
+#                                 "FOREIGN KEY (value_type_id) REFERENCES analysis_value_type (id)\n"
+#                                 ");\n")
 
 ANALYSIS_VALUE_TABLE_CREATION = ("CREATE TABLE IF NOT EXISTS analysis_value (\n"
                                  "id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
                                  "lab_id INTEGER NOT NULL,\n"
-                                 "analysis_item_id INTEGER NOT NULL,\n"
-                                 "is_numeric INTEGER NOT NULL,\n"
+                                 "analysis_type_id INTEGER NOT NULL,\n"
+                                 "is_result_numeric INTEGER NOT NULL,\n"
                                  "result_text TEXT,\n"
                                  "result_value REAL,\n"
-                                 "name TEXT NOT NULL,\n"
+                                 "is_reference_numeric INTEGER NOT NULL,\n"
+                                 "reference TEXT,\n"
                                  "upper_limit REAL,\n"
                                  "lower_limit REAL,\n"
                                  "time_stamp TEXT NOT NULL,\n"
                                  "FOREIGN KEY (lab_id) REFERENCES laboratory (id),\n"
-                                 "FOREIGN KEY (analysis_item_id) REFERENCES analysis_item (id)\n"
+                                 "FOREIGN KEY (analysis_type_id) REFERENCES analysis_value_type (id)\n"
                                  ");\n")
 
-TABLES_QUERIES_LIST = [LABORATORY_TABLE_CREATION, ANALYSIS_VALUE_TYPE_TABLE_CREATION,
-                       ANALYSIS_VALUE_TABLE_CREATION, ANALYSIS_ITEM_TABLE_CREATION]
+TABLES_QUERIES_LIST = [LABORATORY_TABLE_CREATION,
+                       ANALYSIS_VALUE_TYPE_TABLE_CREATION,
+                       ANALYSIS_VALUE_TABLE_CREATION,
+                       # ANALYSIS_ITEM_TABLE_CREATION
+                       ]
--- utils/database/database.py
+++ utils/database/database.py
@@ -1,152 +1,225 @@
 import sqlite3
 from utils.constants.database import *
 from sqlite3 import Error
+from accessify import private, protected
 
 
-def create_connection(path):
-    """ create a database connection to the SQLite database
+class DbConnector:
+    def __init__(self, path):
+        self.path = path
+        self.connection = self.__create_connection(self.path)
+        self.__setup_db(self.connection)
+        self.__select_lab_id_by_name(self.connection, 1)
+
+    @classmethod
+    def __create_connection(cls, path):
+        """
+        create a database connection to the SQLite database
             specified by the db_file
         :param path: database file
         :return: Connection object or None
         """
-    conn = None
-    try:
-        conn = sqlite3.connect(path)
-        print("Connection to SQLite DB successful")
-    except Error as e:
-        print(f"The error {e} occurred")
+        conn = None
+        try:
+            conn = sqlite3.connect(path)
+            print("Connection to SQLite DB successful")
+        except Error as e:
+            print(f"The error {e} occurred")
+        return conn
 
-    return conn
+    @classmethod
+    def __setup_db(cls, connection):
+        """
+        Create tables from tables queries list
+        :param conn: the Connection object
+        :return:
+        """
+        cur = connection.cursor()
 
+        for query in TABLES_QUERIES_LIST:
+            try:
+                cur.execute(query)
+                print('Query executed successfully')
 
-# def execute_query(connector, query, value=False):
-#     cursor = connector.cursor()
-#     try:
-#         if value:
-#             cursor.execute(query, value)
-#         else:
-#             cursor.execute(query)
-#         connector.commit()
-#         print('Query executed successfully')
-#
-#     except Error as e:
-#         print(f'The error {e} occurred')
+            except Error as e:
+                print(f'The error {e} occurred')
 
+        connection.commit()
 
-# def execute_many_query(connector, query, value=False):
-#     cursor = connector.cursor()
-#     try:
-#         if value:
-#             cursor.executemany(query, value)
-#         else:
-#             cursor.executemany(query)
-#         connector.commit()
-#         print('Query executed successfully')
-#
-#     except Error as e:
-#         print(f'The error {e} occurred')
+    def select_all_analysis_types(self):
+        """
+        Query all rows in the analysis table
+        :param conn: the Connection object
+        :return:
+        """
+        cur = self.connection.cursor()
+        query = "SELECT name, abbreviation FROM analysis_value_type"
 
-
-def setup_db(conn):
-    """
-    Create tables from tables queries list
-    :param conn: the Connection object
-    :return:
-    """
-    cur = conn.cursor()
-
-    for query in TABLES_QUERIES_LIST:
         try:
             cur.execute(query)
             print('Query executed successfully')
 
         except Error as e:
             print(f'The error {e} occurred')
-    conn.commit()
+
+        # коммит не нужен, т.к. бд никак не изменяется
+        # conn.commit()
+
+        # Создаем список кортежей вида [(name, abb), (name, abb)]
+        rows = cur.fetchall()
+        return rows
+
+    @classmethod
+    def __select_analysis_id_by_type(cls, connection, analysis_type):
+        """
+        Query analysis id by type. Internal function
+        :param analysis_type: name of the selected analysis type
+        :return: id of the type
+        """
+        cur = connection.cursor()
+        query = "SELECT id FROM analysis_value_type WHERE name = (?)"
+
+        try:
+            cur.execute(query, (analysis_type,))
+            print('Query executed successfully')
+
+        except Error as e:
+            print(f'The error {e} occurred')
+
+        # conn.commit()
+        rows = cur.fetchone()
+
+        return rows[0][0]
+
+    def select_analysis_info_by_type(self, analysis_type):
+        """
+        Query all analysis of the selected type. Public function
+        :param conn: the Connection object
+        :param analysis_type: name of the selected analysis type
+        :return: all information about selected analysis
+        """
+        cur = self.connection.cursor()
+        query = "SELECT * FROM analysis_value WHERE analysis_type_id = (?)"
+        analysis_type = self.__select_analysis_id_by_type(self.connection,
+                                                          analysis_type)
+
+        try:
+            cur.execute(query, (analysis_type,))
+            print('Query executed successfully')
+
+        except Error as e:
+            print(f'The error {e} occurred')
+
+        # conn.commit()
+        rows = cur.fetchall()
+
+        return rows
+
+    @classmethod
+    def __select_lab_id_by_name(cls, connection, name):
+        """
+        Query analysis id by type. Internal function
+        :param analysis_type: name of the selected analysis type
+        :return: id of the type
+        """
+        cur = connection.cursor()
+        query = "SELECT id FROM laboratory WHERE name = (?)"
+
+        try:
+            cur.execute(query, (name,))
+            print('Query executed successfully')
+
+        except Error as e:
+            print(f'The error {e} occurred')
+        # conn.commit()
+        rows = cur.fetchone()
+        print(rows)
+        return rows if rows is None else rows[0][0]
+
+    def insert_lab(self, lab):
+        if self.__select_lab_id_by_name is not None:
+            return
+
+        cur = self.connection.cursor()
+        query = f"INSERT INTO laboratory (name) VALUES ({lab})"
+
+        try:
+            cur.execute(query)
+            print('Query executed successfully')
+            self.connection.commit()
+        except Error as e:
+            print(f'The error {e} occurred')
+
+    @classmethod
+    def __select_analysis_value_type_id_by_name(cls, connection, name):
+        """
+        Query analysis id by type. Internal function
+        :param analysis_type: name of the selected analysis type
+        :return: id of the type
+        """
+        cur = connection.cursor()
+        query = "SELECT id FROM analysis_value_type WHERE name = (?)"
+
+        try:
+            cur.execute(query, (name,))
+            print('Query executed successfully')
+
+        except Error as e:
+            print(f'The error {e} occurred')
+        # conn.commit()
+        rows = cur.fetchone()
+        print(rows)
+        return rows if rows is None else rows[0][0]
+
+    def insert_analysis_value_type(self, name):
+        if self.__select_analysis_value_type_id_by_name(self.connection, name)\
+                is not None:
+            return
+
+        cur = self.connection.cursor()
+        query = f"INSERT INTO laboratory (name) VALUES ({name})"
+
+        try:
+            cur.execute(query, (id,))
+            print('Query executed successfully')
+            self.connection.commit()
+        except Error as e:
+            print(f'The error {e} occurred')
+
+    def insert_analysis_value(self, data):
+        # ["laboratory_id", "analysis_value_type_id", "is_numeric",
+        # "result_text", "result_value", "unit", "limit_is_numeric",
+        # "reference", "upper_limit", "lower_limit", "time_stamp"]
+
+        cur = self.connection.cursor()
+        # query = f"INSERT INTO laboratory (name) VALUES ({name})"
+
+        try:
+            # cur.execute(query, (id,))
+            print('Query executed successfully')
+
+        except Error as e:
+            print(f'The error {e} occurred')
+    def delete(self, table, data):
+        return
 
 
-def select_all_analysis_types(conn):
-    """
-    Query all rows in the analysis table
-    :param conn: the Connection object
-    :return:
-    """
-    cur = conn.cursor()
-    query = "SELECT name, abbreviation FROM analysis_value_type"
+db = DbConnector(DATABASE_PATH)
 
-    try:
-        cur.execute(query)
-        print('Query executed successfully')
+data_sample_new = [["laboratory_name", "analysis_value_type_name", "is_numeric",
+                    "result_text", "result_value", "unit", "limit_is_numeric",
+                    "reference", "upper_limit", "lower_limit", "time_stamp"],
+                   [], []
+                   ]
 
-    except Error as e:
-        print(f'The error {e} occurred')
-    conn.commit()
-    rows = cur.fetchall()
+data_sample_for_testing = \
+    [
+        ["днком", "ВПЧ типы 51,56", 0, "не обнаружено",
+         "LgВПЧ/10^5 эпит.клеток", 0, "не обнаружено", "2022-01-27"],
+        ["гемотест", "витамин А", 1, 0.5, "мкг/мл", 1, 0.2, 0.8, "2023-05-14"]
+    ]
 
-    return rows
-
-
-def select_analysis_by_type(conn, analysis_type):
-    """
-    Query analysis by type
-    :param conn: the Connection object
-    :param analysis_type: type of the selected analysis
-    :return: result rows
-    """
-    cur = conn.cursor()
-    query = "SELECT * FROM analysis_value WHERE analysis_item_id = (?)"
-
-    try:
-        cur.execute(query, (analysis_type,))
-        print('Query executed successfully')
-
-    except Error as e:
-        print(f'The error {e} occurred')
-    conn.commit()
-    rows = cur.fetchall()
-    return rows
-
-
-def insert_analysis(connector, table, value):
-    val_str = "?," * (len(value) - 1)
-
-    query = f"INSERT INTO {table} VALUES ({val_str})"
-
-    if type(value[0]) not in [tuple, list]:
-        execute_query(connector, query, value)
-    else:
-        execute_many_query(connector, query, value)
-
-    return True
-
-
-def delete(connector, table, data):
-    return
-
-
-connection = create_connection(DATABASE_PATH)
-setup_db(connection)
-# select_by_analysis_type(connection, 5)
-
-data_sample = {"laboratory": [["name1"], ["name2"]],
-               "analysis_value_type": [["name1", "abbreviation1"],
-                                       ["name2", "abbreviation2"]],
-               "analysis_item": [["name1", '']],
-               "analysis_value": [
-                   ["lab_id1", "analysis_item_id1", "is_numeric1",
-                    "result_text1", "result_value", "name",
-                    "upper_limit", "lower_limit", "time_stamp"]]
-               }
-
-data_sample_new = {[""], [], []
-                   }
-
-    # "laboratory": [["name1"], ["name2"]],
-    #            "analysis_value_type": [["name1", "abbreviation1"],
-    #                                    ["name2", "abbreviation2"]],
-    #            "analysis_item": [["name1", '']],
-    #            "analysis_value": [
-    #                ["lab_id1", "analysis_item_id1", "is_numeric1",
-    #                 "result_text1", "result_value", "name",
-    #                 "upper_limit", "lower_limit", "time_stamp"]]
-    #            }
+# Нужно добавить таблицу для перевода единиц вида:
+# id_1 | id_2 | factor - необходимо заполнить заранее
+# И таблицу с сопоставлением единиц измерения и id:
+# id | name
