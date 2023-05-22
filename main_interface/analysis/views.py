9406695e22ded57eac213b7fb4a84389cc96cb40
veta <ivetasand@gmail.com>
2023-05-22 11:35:52 +0300

    numeric chart is ready somehow

--- main_interface/analysis/views.py
+++ main_interface/analysis/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from utils.database.interface import DbInterface
+import json
 
 
 # Create your views here.
@@ -17,15 +18,20 @@ def analysis_detail_view(request, analysis_type):
         lower_limits = [sublist[7] for sublist in obj]
         upper_limits = [sublist[8] for sublist in obj]
         dates = [sublist[9] for sublist in obj]
+        results_dates_lower_upper = zip(results, dates, lower_limits,
+                                        upper_limits)
+        # results_dates_lower_upper_json = json.dumps(results_dates_lower_upper)
 
         context = {
             'analysis_type_name': obj[0][2],
             'units': obj[0][-1],
+            'results_dates_lower_upper': results_dates_lower_upper,
             'results': results,
             'lower_limits': lower_limits,
             'upper_limits': upper_limits,
             'dates': dates
         }
+
         return render(request, "analysis/detail_numeric.html", context)
 
     else:
@@ -56,7 +62,7 @@ def analysis_list_view(request):
             # reference_lower_value,reference_upper_value, date, units]
             ["гемотест", "витамин А", 1, 0.5, 0.2, 0.8, "2023-05-14",
              "unit_name2"],
-            ["гемотест", "витамин А", 1, 0.5, 0.2, 0.8, "2023-06-14",
+            ["гемотест", "витамин А", 1, 0.6, 0.2, 0.8, "2023-06-14",
              "unit_name2"],
             ["днком", "ВПЧ типы 51,56", 0, "обнаружено", "не обнаружено",
              "2023-01-27"],
