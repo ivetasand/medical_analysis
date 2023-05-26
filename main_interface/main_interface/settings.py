87634c2a90fb329f88c4d0cde5c599e833ea7952
veta <ivetasand@gmail.com>
2023-05-26 20:12:08 +0300

    emm

--- main_interface/main_interface/settings.py
+++ main_interface/main_interface/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=!z!2o)sx_ueqj#n&n9!6xor3qu+x*52a6+5s0)wfs!4+gv(st'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app.apps.AppConfig',
     'pages.apps.AppConfig',
     'analysis.apps.AppConfig',
     'login.apps.AppConfig',
